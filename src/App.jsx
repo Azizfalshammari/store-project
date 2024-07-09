4259cfa56bbde7687b1803f2b2d1e79002291ccf
Yazeed0xx <yazeedthunyan@gmail.com>
2024-07-09 20:22:06 +0300

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import { useState } from "react";
 import "./App.css";
+import Cheack from "./Pages/Cart";
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Details from "./Pages/Details";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Cheack />,
+  },
+  {
+    path: "/details",
+    element: <Details />,
+  },
+]);
 function App() {
   return (
     <>
-      <div>Git test</div>
+      <RouterProvider router={router} />
     </>
   );
 }
