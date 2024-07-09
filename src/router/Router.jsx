7cfa4ffa00e96a55bfd324bbb52f7baeb7a4e4b4
Yazeed0xx <yazeedthunyan@gmail.com>
2024-07-09 23:14:28 +0300

    Merging

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,43 +1,39 @@
-
-import {createBrowserRouter,RouterProvider} from "react-router-dom";
-import Home from "../pages/Home";
-import Details from "../pages/Details";
-import Signup from "../pages/Signup";
-import Login from "../pages/Login";
-import Cheackout from "../pages/Cheackout";
-import Cart from "../pages/Cart";
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import Home from "../Pages/Home";
+import Details from "../Pages/Details";
+// import Signup from "../Pages/Signup";
+// import Login from "../Pages/Login";
+// import Cheackout from ".../Pages/Cheackout";
+import Cart from "../Pages/Cart";
 
 function Router() {
-    const router = createBrowserRouter([
-        {
-          path: "/",
-          element: <Home/>
-        },
-        {
-          path: "/details/:id",
-          element: <Details/>
-        },
-        {
-            path: "/checkout",
-            element: <Cheackout/>
-          },
-        {
-            path: "/cart",
-            element: <Cart/>
-          },
-          {
-            path: "/signup",
-            element: <Signup/>
-          },
-          {
-            path: "/login",
-            element: <Login/>
-          },
-      ]);
-      return(
-        <RouterProvider router={router} />
-      )
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/details/:id",
+      element: <Details />,
+    },
+    // {
+    //   path: "/checkout",
+    //   element: <Cheackout />,
+    // },
+    {
+      path: "/cart",
+      element: <Cart />,
+    },
+    // {
+    //   path: "/signup",
+    //   element: <Signup />,
+    // },
+    // {
+    //   path: "/login",
+    //   element: <Login />,
+    // },
+  ]);
+  return <RouterProvider router={router} />;
 }
 
-export default Router
+export default Router;
