1e8fb731d7f35c3ce1a0cdc9a657fce7f2fa7194
Yazeed0xx <yazeedthunyan@gmail.com>
2024-07-10 14:57:00 +0300

    a little merge

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,35 +1,29 @@
+import React from "react";
+import Cheack from "../Pages/Cart";
+
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "../Pages/Home";
 import Signup from "../Pages/Signup";
 import Login from "../Pages/Login";
 import Details from "../Pages/Details";
-import Cart from "../Pages/Cart";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Cheack />,
+  },
+  {
+    path: "/details",
+    element: <Details />,
+  },
+]);
 
 function Router() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Home />,
-    },
-
-    {
-      path: "/signup",
-      element: <Signup />,
-    },
-    {
-      path: "/login",
-      element: <Login />,
-    },
-    {
-      path: "/details/:id",
-      element: <Details />,
-    },
-    {
-      path: "/cart",
-      element: <Cart />,
-    },
-  ]);
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      {" "}
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default Router;
