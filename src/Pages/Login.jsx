4e94c6a88bc9d921eece79be5c9f9be5f9223988
Azizfalshammari <azizfalshammari.com>
2024-07-10 09:39:00 +0300

    adcarousel and log in page

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,21 +1,50 @@
 import React, { useState } from "react";
-
+import axios from "axios";
+import AdCarousel from "../Components/AdCarousel";
 function LoginForm() {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    // Handle login logic here (e.g., API call, validation)
-    console.log("Logging in with:", { username, email, password });
+    axios
+      .get("https://6685a3abb3f57b06dd4d6580.mockapi.io/users")
+      .then((res) => {
+        const user = res.data.find(
+          (user) =>
+            user.username === username &&
+            user.email === email &&
+            user.password === password
+        );
+        if (user) {
+          console.log("User verified:", user);
+          // Handle successful login (e.g., redirect to another page)
+        } else {
+          setError("Invalid username, email, or password");
+        }
+      })
+      .catch((err) => {
+        console.error("Error fetching users:", err);
+        setError(
+          "An error occurred while verifying credentials. Please try again."
+        );
+      });
   };
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col w-full max-w-xs">
-        <h2 className="text-center text-xl font-bold mb-4">Login</h2>
-        <form onSubmit={handleSubmit}>
+    <div className="flex  items-center justify-center min-h-screen bg-gray-100">
+            <AdCarousel />
+
+      <div className="flex flex-col items-center justify-center w-full max-w-md px-8 py-4 bg-white shadow-md rounded-lg">
+        <img src="/path/to/logo.png" alt="Logo" className="w-24 h-24 mb-4" />{" "}
+        <p className="mt-4 text-gray-600">
+          Welcome to our application. Please log in to continue.
+        </p>
+        <h2 className="text-2xl font-bold mb-4">Login</h2>
+        {error && <div className="mb-4 text-red-500 text-sm">{error}</div>}
+        <form onSubmit={handleSubmit} className="w-full">
           <div className="mb-4">
             <label className="block text-gray-700 text-sm font-bold mb-2">
               Username
